a2b77b7211374fc10ecab168097ec6ce75b2502a
Petr Povazhniy <ess_service@hotmail.com>
2016-08-19 19:02:41 +0300

    Lets start

---
+++ spWeb/App_Start/IdentityConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly:OwinStartup(typeof(spWeb.IdentityConfig))]
+namespace spWeb
+{
+    public class IdentityConfig
+    {
+         public void Configuration(IAppBuilder app) { }
+    }
+}
